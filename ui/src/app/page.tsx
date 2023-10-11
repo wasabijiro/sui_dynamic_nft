998eed549114573dc71659924889a0ffd82ed178
wasabijiro <shushu011023@gmail.com>
2023-10-11 11:24:00 +0900

    connect ui folder to git

---
+++ ui/src/app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { ConnectButton } from "@suiet/wallet-kit";
+
+export default function Home() {
+  return (
+    <div>
+      <header>
+        <ConnectButton />
+      </header>
+    </div>
+  );
+}
